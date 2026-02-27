97eed1c5f0f89fe485ab325df049684cbc43e990
DESKTOP-N8P68KG\alexe <alexeybobrov1@mail.ru>
2026-02-27 16:33:41 +0700

    Инициализированы проекты для разработки и тестирования расширений

---
+++ TestListExtensions/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestListExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3 };
+            List<int> list = new List<int> { 1, 2, 3 };
+            Span<int> span = CollectionsMarshal.AsSpan(list);
+
+
+        }
+    }
+}
