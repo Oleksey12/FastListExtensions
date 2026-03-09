3c77d9038dc3ea45c00d8ebd68a027d0e404a286
DESKTOP-N8P68KG\alexe <alexeybobrov1@mail.ru>
2026-03-09 15:27:12 +0700

    Написана логика для извлечения элемента из списка

---
+++ TestListExtensions/TestHelperExtensions.cs
@@ -0,0 +1,52 @@
+﻿namespace ListExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Numerics;
+    using System.Runtime.CompilerServices;
+    using System.Runtime.InteropServices;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
+
+    public static class TestHelperExtensions
+    {
+        private enum Elements
+        {
+            All = -1
+        }
+        public static T Pop<T>(this List<T> inputList)
+        {
+            if (inputList == null)
+            {
+                throw new NullReferenceException("List cannot be null!");
+            }
+
+            if (inputList.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Can't pop from empty list!");
+            }
+
+            int lastIndex = inputList.Count - 1;
+            T item = inputList[lastIndex];
+            inputList.RemoveAt(lastIndex);
+            return item;
+        }
+
+        public static T PopFirst<T>(this List<T> inputList)
+        {
+            if (inputList == null)
+            {
+                throw new NullReferenceException("List cannot be null!");
+            }
+
+            if (inputList.Count == 0)
+            {
+                throw new IndexOutOfRangeException("Can't pop from empty list!");
+            }
+
+            int index = 0;
+            T item = inputList[index];
+            inputList.RemoveAt(index);
+            return item;
+        }
+    }
+}
